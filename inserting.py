3cf761da83806e4750524f9443f79812fa0cae9f
Evelyn Ting <evelynting610@users.noreply.github.com>
2015-02-10 17:00:34 -0500

    Update inserting.py

--- inserting.py
+++ inserting.py
@@ -25,6 +25,7 @@ def insert_participant():
         "seeking":request.form['seeking']
         }
     participants.insert(participant)
+    return render_template('completed.html')
 
 if __name__ == '__main__':
     app.debug=True          #restarts every time you change code
